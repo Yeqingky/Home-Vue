ea7fbd9e1a430c4919dc4a2b2df3fcac04956f4f
YeQingqa <i@yeqing.net>
2025-06-06 19:30:36 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <footer>
     <span>© 2024 Made in <a href="/" target="_blank">{{ userName }}</a></span>
     <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
-    <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
+    <a v-if="policenumber" :href="`https://icp.gov.moe/?keyword=20259980`" target="_blank" class="police_link">
       <span class="police_img"></span> {{ policenumber }}
     </a>
   </footer>
